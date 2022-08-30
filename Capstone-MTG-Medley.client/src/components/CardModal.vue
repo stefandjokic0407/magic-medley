284f0afbfb87472ef520d5ea304b252703cdbffb
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-08-30 16:34:37 -0600

    async

--- Capstone-MTG-Medley.client/src/components/CardModal.vue
+++ Capstone-MTG-Medley.client/src/components/CardModal.vue
@@ -1,8 +1,18 @@
 <template>
-
-{{card.name}}
-<img class="img-fluid selectable shadow" :src=card.image_uris.normal :alt="card.name">
-
+<div class="card mb-3" style="max-width: 1100px;">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <img :src=card.image_uris.normal class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
@@ -11,14 +21,13 @@ import { Card } from "../models/Card";
 
 export default {
   props: { card: { type: Card, required: true } },
-setup() {
-  return {
-  };
+  setup() {
+    return {
+    };
 
   }
 }
 </script>
 
-<style> 
-
+<style>
 </style>
