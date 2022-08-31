a0bf1daab42a964ec7f542ddab14e33515c81f71
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-08-31 09:53:35 -0600

    changes

--- Capstone-MTG-Medley/server/db/DbContext.js
+++ Capstone-MTG-Medley/server/db/DbContext.js
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
+import { CardSchema } from '../models/Card.js';
+import { DeckSchema } from '../models/Deck.js';
+import { DeckCardSchema } from '../models/DeckCard.js';
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+  Decks = mongoose.model('Deck', DeckSchema);
+
+  Cards = mongoose.model('Card', CardSchema);
+
+  DeckCards = mongoose.model('DeckCard', DeckCardSchema);
+
+
 }
 
 export const dbContext = new DbContext()
