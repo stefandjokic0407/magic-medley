4f45b18befb5c41065eaea6cbe3d24f0421d62a9
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-08-31 11:09:43 -0600

    cardsController (on accountController) complete

--- Capstone-MTG-Medley/server/controllers/AccountController.js
+++ Capstone-MTG-Medley/server/controllers/AccountController.js
@@ -1,5 +1,6 @@
 import { Auth0Provider } from '@bcwdev/auth0provider'
 import { accountService } from '../services/AccountService'
+import { cardsService } from "../services/CardsService.js"
 import BaseController from '../utils/BaseController'
 
 export class AccountController extends BaseController {
@@ -9,6 +10,11 @@ export class AccountController extends BaseController {
       .use(Auth0Provider.getAuthorizedUserInfo)
       .get('', this.getUserAccount)
       .put('', this.edit)
+      .get('/cards', this.getAllAccountCards)
+      .get('/cards/:cardId', this.getCardById)
+      .post('/cards', this.createCard)
+      .put('/cards/:cardId', this.updateCard)
+      .delete('/cards/:cardId', this.deleteCard)
   }
 
   async getUserAccount(req, res, next) {
@@ -28,4 +34,52 @@ export class AccountController extends BaseController {
       next(error)
     }
   }
+
+async getAllAccountCards(req, res, next){
+  try {
+      const cards = await cardsService.getAllAccountCards(req.userInfo.id)
+      return res.send(cards)
+  } catch (error) {
+      next(error)
+  }
+}
+
+async getCardById(req, res, next) {
+  try {
+    const card = await cardsService.getCardById(req.params.cardId, req.userInfo.id)
+    return res.send(card)
+  } catch (error) {
+    next(error)
+  }
+}
+
+async createCard(req, res, next) {
+  try {
+    req.body.accountId = req.userInfo.Id
+    const card = await cardsService.createCard(req.body)
+    return res.send(card)
+  } catch (error) {
+    
+  }
+}
+
+async updateCard(req, res, next) {
+  try {
+    let cardData = req.body
+    cardData.accountId = req.userInfo.Id
+    const card = await cardsService.updateCard(req.params.cardId, cardData)
+    return res.send(card)
+  } catch (error) {
+    next(error)
+  }
+}
+async deleteCard(req, res, next) {
+  try {
+    const response = await cardsService.deleteCard(req.params.cardId, req.userInfo.id)
+    return res.send(response)
+  } catch (error) {
+    next(error)
+  }
+}
+
 }
