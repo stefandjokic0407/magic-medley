6535abb1ca86f4046540a1ae3913ae901f92eb50
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-08-31 10:41:29 -0600

    cardscontroller functions up

--- Capstone-MTG-Medley/server/controllers/CardsController.js
+++ Capstone-MTG-Medley/server/controllers/CardsController.js
@@ -1,12 +1,18 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
 
 
 export class CardsController extends BaseController{
     constructor(){
-        super('')
+        super('api/account/cards')
         this.router
+        .use(Auth0Provider.getAuthorizedUserInfo)
+        .get('', this.getAllAccountCards)
+        .get('/:cardId', this.getCardById)
+        .post('', this.saveCard)
+        .put('/:cardId', this.updateCard)
+        .delete('/:cardId', this.deleteCard)
     }
-// random comment
 
 }
 
