04cd884907053bb09ec25f98bc9c30bf0580a3aa
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-08-31 17:04:54 -0600

    updated card model

--- Capstone-MTG-Medley.client/src/models/Card.js
+++ Capstone-MTG-Medley.client/src/models/Card.js
@@ -4,24 +4,26 @@ export class Card {
     this.artist = data.artist || ""
     this.cmc = data.cmc || null
     this.colors = data.colors || []
+    this.count = data.count || null
     this.flavor_text = data.flavor_text || ""
     this.id = data.id || ""
-    this.image_uris = data.image_uris || ""
+    this.accountId = data.accountId 
+    this.image_uris = data.image_uris || {}
     // this.image_normal = data.image_uris.normal || ""
     this.layout = data.layout || ""
-    this.legalities = data.legalities || ""
+    this.legalities = data.legalities || {}
     this.mana_cost = data.mana_cost || ""
     this.name = data.name || ""
     this.oracleId = data.oracle_id || ""
     this.oracle_text = data.oracle_text || ""
-    this.power = data.power || ""
-    this.prices = data.prices|| ""
-    this.purchase_uris = data.purchase_uris || ""
+    this.power = data.power || null
+    this.prices = data.prices|| {}
+    this.purchase_uris = data.purchase_uris || {}
     this.reprint = data.reprint || false
     this.reserved = data.reserved || false
     this.set = data.set || ""
     this.set_name = data.set_name || ""
-    this.toughness = data.toughness || ""
+    this.toughness = data.toughness || null
     this.type_line = data.type_line || ""
   }
 }
