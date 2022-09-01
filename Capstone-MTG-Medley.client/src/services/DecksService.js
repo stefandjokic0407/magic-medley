e3eef7327deb40294f3d97365af4241f1111c626
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-01 11:57:17 -0600

    decksService

--- Capstone-MTG-Medley.client/src/services/DecksService.js
+++ Capstone-MTG-Medley.client/src/services/DecksService.js
@@ -1,5 +1,13 @@
+import { AppState } from "../AppState.js"
+import { logger } from "../utils/Logger.js"
+import { api } from "./AxiosService.js"
 
 class DecksService{
+    async createDeck(newDeck){
+    const res = await api.post('/api/decks')
+    logger.log('Creating Deck', res.data)
+    AppState.decks = res.data
+    }
 
 }
 
