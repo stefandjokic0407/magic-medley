161fdd222d336427462299b77d53a60c03bd0eb5
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-01 15:17:46 -0600

    deckform and create deck up

--- Capstone-MTG-Medley.client/src/pages/CollectionPage.vue
+++ Capstone-MTG-Medley.client/src/pages/CollectionPage.vue
@@ -4,19 +4,22 @@
   </header>
   <div class="row">
     <!-- add deck component -->
-    <h3 class="col-12">My Decks <button @click="createDeck" class="btn text-black lighten-30 selectable text-uppercase square buttonPadding">Create Deck</button> </h3>
+    <h3 class="col-12">My Decks <button data-bs-toggle="modal" data-bs-target="#deck-form"
+        class="btn text-black lighten-30 selectable text-uppercase square buttonPadding">Create Deck</button> </h3>
   </div>
   <div class="row">
     <h3 class="col-12">My Collection</h3>
     <div v-for="c in cards" :key="c.id">
       <div class="col-2 card selectable">
-        <CollectionCard :card="c"/>
+        <CollectionCard :card="c" />
       </div>
     </div>
   </div>
+  <DeckForm />
 </template>
 
 <script>
+
 import { computed } from '@vue/reactivity';
 import { onMounted } from 'vue';
 import { AppState } from '../AppState.js';
@@ -25,41 +28,32 @@ import { logger } from '../utils/Logger.js';
 import Pop from '../utils/Pop.js';
 import SearchedCards from '../components/SearchedCards.vue';
 import CollectionCard from '../components/CollectionCard.vue';
-
+import DeckForm from "../components/DeckForm.vue";
 
 export default {
-    setup() {
-        async function getAccountCards() {
-            try {
-                const accountId = AppState.account.id;
-                await cardsService.getAccountCards(accountId);
-            }
-            catch (error) {
-                logger.log("[getting all cards]", error);
-                Pop.error(error);
-            }
-        }
-        async function createDeck(newDeck) {
-            try {
-                await decksService.createDeck(newDeck);
-            }
-            catch (error) {
-                logger.log("[Creating Deck]", error);
-                Pop.error(error);
-            }
-        }
-        onMounted(() => {
-            getAccountCards();
-        });
-        return {
-            cards: computed(() => AppState.collection),
-            decks: computed(() => AppState.decks)
-        };
-    },
-    components: { SearchedCards, CollectionCard }
+  setup() {
+    async function getAccountCards() {
+      try {
+        const accountId = AppState.account.id;
+        await cardsService.getAccountCards(accountId);
+      }
+      catch (error) {
+        logger.log("[getting all cards]", error);
+        Pop.error(error);
+      }
+    }
+
+    onMounted(() => {
+      getAccountCards();
+    });
+    return {
+      cards: computed(() => AppState.collection),
+      decks: computed(() => AppState.decks)
+    };
+  },
+  components: { SearchedCards, CollectionCard, DeckForm }
 }
 </script>
 
 <style>
-
 </style>
