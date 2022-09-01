f9f1cc0298d7a63653f770a7c01a94c9c1692204
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-01 09:02:00 -0600

    filter

--- Capstone-MTG-Medley.client/src/services/CardsService.js
+++ Capstone-MTG-Medley.client/src/services/CardsService.js
@@ -16,13 +16,12 @@ class CardsService {
     // if (AppState.searchByText) {}
 
     const colorFilter = '+color%3D' + filterTerm
-    const rarityFilter = 'rarity%3A' + filterTerm
     // console.log(colorFilter, searchTerm, filterTerm);
-    const res = await search.get(searchTerm + rarityFilter + colorFilter)
+    const res = await search.get(searchTerm + colorFilter)
     AppState.searchedCards = res.data.data.map(c => new Card(c))
     // AppState.cards = res.data.
   }
-  
+
   async searchBarGet(searchTerm) {
     const res = await search.get(searchTerm)
     AppState.searchedCards = res.data.data.map(c => new Card(c))
@@ -59,7 +58,7 @@ class CardsService {
     console.log('Getting card by oracle', AppState.oracleCard)
   }
 
-  async changePage(url){
+  async changePage(url) {
     const res = await search.get(AppState.nextPage)
     AppState.searchedCards = res.data.data.map(c => new Card(c))
     AppState.nextPage = res.data.next_page
