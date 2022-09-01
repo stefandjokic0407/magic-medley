8513ae9c154396625cb058d8dc9004f431d69f3d
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-01 13:07:37 -0600

    card creation on server now overwrites images

--- Capstone-MTG-Medley/server/controllers/CollectionsController.js
+++
@@ -1,9 +0,0 @@
-import BaseController from "../utils/BaseController.js";
-
-
-export class CollectionsController extends BaseController{
-    constructor(){
-        super('')
-        this.router
-    }
-}
