82661904076eac4d2a4a61a9a2d2e78c3f8331be
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-01 20:57:16 -0600

    sync

--- Capstone-MTG-Medley.client/src/components/CollectionCard.vue
+++ Capstone-MTG-Medley.client/src/components/CollectionCard.vue
@@ -1,6 +1,7 @@
 <template>
 
-  <div>
+  <div @click="getCardByOracle() && reset(card)" type="button" data-bs-toggle="modal"
+    :data-bs-target="'#collectionCardModal' + card.id" class="mt-4 shadow rotated">
     <div v-if="card.image_uris?.normal">
       <img class="img-fluid position-relative shadow cardsBg" :src=card.image_uris?.normal>
     </div>
@@ -8,34 +9,72 @@
       <img class="img-fluid shadow cardsBg"
         src="https://c1.scryfall.com/file/scryfall-card-backs/large/59/597b79b3-7d77-4261-871a-60dd17403388.jpg?1561757712">
     </div>
-    <button v-if="activeDeck" class="btn add-to-deck" @click="createDeckCard(card.cardId)"><i title="add to deck" class="mdi mdi-plus-circle"></i></button>
+    <button v-if="activeDeck" class="btn add-to-deck" @click="createDeckCard(card.cardId)"><i title="add to deck"
+        class="mdi mdi-plus-circle"></i></button>
   </div>
+  <Modal :id="'collectionCardModal' + card.id">
+    <CardModal :card="card" />
+  </Modal>
+
+
+  <!-- <div  @click="getCardByOracle() && reset(card)"  type="button" data-bs-toggle="modal" :data-bs-target="'#cardModal' + card.id"
+class="mt-4 shadow rotated">
+<div class="row">
+  <div v-if="card.image_uris.normal" class="col-12 ">
+    <img class="img-fluid shadow cardsBg" :src=card.image_uris.normal>
+  </div>
+  <div v-else class="col-12">
+    <img class="img-fluid shadow cardsBg"
+      src="https://c1.scryfall.com/file/scryfall-card-backs/large/59/597b79b3-7d77-4261-871a-60dd17403388.jpg?1561757712">
+  </div>
+</div>
+</div>
+<Modal :id="'cardModal' + card.id">
+<CardModal :card="card" />
+</Modal> -->
 
 </template>
 <script>
 import { computed } from '@vue/reactivity';
 import { AppState } from '../AppState.js';
 import { Card } from '../models/Card.js';
+import { cardsService } from "../services/CardsService";
+import { logger } from "../utils/Logger";
+import Pop from "../utils/Pop";
 
 export default {
   props: { card: { type: Card, required: true } },
 
-  setup() {
+
+  setup(props) {
     return {
       cards: computed(() => AppState.collection),
-      activeDeck: computed(() => AppState.activeDeck)
+      activeDeck: computed(() => AppState.activeDeck),
+      reset() {
+        AppState.activeCard = props.card
+        console.log('Active Card:', props.card)
+      },
+      async getCardByOracle() {
+        try {
+          console.log(" Id", props.card.oracleId);
+          await cardsService.getCardByOracle(props.card.oracle_id);
+        }
+        catch (error) {
+          logger.error(error);
+          Pop.toast(error.message, "error");
+        }
+      },
     }
   }
 }
 </script>
 
 <style>
-  .add-to-deck{
-    position: absolute;
-  }
+.add-to-deck {
+  position: absolute;
+}
 
-  .position-relative{
-    position: relative;
-  }
-
+.position-relative {
+  position: relative;
+}
 </style>
