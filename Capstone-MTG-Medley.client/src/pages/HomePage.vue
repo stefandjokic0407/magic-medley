e1921d5487654154ce4727fb43e11a507e8cee0d
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-02 12:41:14 -0600

    and

--- Capstone-MTG-Medley.client/src/pages/HomePage.vue
+++ Capstone-MTG-Medley.client/src/pages/HomePage.vue
@@ -31,6 +31,69 @@
       <button type="button" class="btn" data-bs-dismiss="offcanvas" aria-label="Close"> <i
           class="mdi mdi-close text-danger fs-2" title="Close"></i></button>
     </div>
+    <div class="offcanvas-body">
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+              aria-expanded="true" aria-controls="collapseOne">
+              <div class="p-2 text-uppercase selectable no-select">FAQ</div>
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingTwo">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              <div class="p-2 text-uppercase selectable no-select">Contact Us</div>
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingThree">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+              data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <div class="p-2 text-uppercase selectable no-select">External Links</div>
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse
+              plugin adds the appropriate classes that we use to style each element. These classes control the overall
+              appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom
+              CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+              <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    </div>
   </div>
 
   <div class="offcanvas offcanvas-end offcanvas-style" tabindex="-1" id="offcanvasRight"
@@ -39,6 +102,9 @@
       <h5 class="offcanvas-title" id="offcanvasLabel">Basics</h5>
       <button type="button" class="btn" data-bs-dismiss="offcanvas" aria-label="Close"> <i
           class="mdi mdi-close text-danger fs-2" title="Close"></i></button>
+    </div>
+    <div class="offcanvas-body">
+      Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
     </div>
   </div>
 
@@ -58,26 +124,26 @@ import FooterNav from "../components/FooterNav.vue";
 
 export default {
   setup() {
-    async function getRandomCard() {
-      try {
-        let card = await cardsService.getRandomCard();
-      }
-      catch (error) {
-        logger.error("[Getting Random Card]", error);
-        Pop.toast(error.message, "error");
-      }
-    }
-    async function getSymbols() {
-      try {
-        await symbolsService.getSymbols();
-      }
-      catch (error) {
-        logger.error(error);
-        Pop.toast(error.message, "error");
-      }
-    }
+    // async function getRandomCard() {
+    //   try {
+    //     let card = await cardsService.getRandomCard();
+    //   }
+    //   catch (error) {
+    //     logger.error("[Getting Random Card]", error);
+    //     Pop.toast(error.message, "error");
+    //   }
+    // }
+    // async function getSymbols() {
+    //   try {
+    //     await symbolsService.getSymbols();
+    //   }
+    //   catch (error) {
+    //     logger.error(error);
+    //     Pop.toast(error.message, "error");
+    //   }
+    // }
     onMounted(() => {
-      getSymbols();
+      // getSymbols();
     });
     return {
       card: computed(() => AppState.card),
@@ -122,11 +188,11 @@ img {
 }
 
 .offcanvas-style {
-  background: rgb(0 0 0 / 75%);
+  background: transparent;
   backdrop-filter: blur(4px);
-  color: white;
-  border-radius: 3px;
+  border: none;
   width: 20vw;
-  height: 75vh;
+  text-shadow: none;
+  height: fit-content;
 }
 </style>
