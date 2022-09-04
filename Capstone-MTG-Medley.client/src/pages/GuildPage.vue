ae64653bf22db1d3b0661990bdb41797162f7a50
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-04 14:37:19 -0600

    maps

--- Capstone-MTG-Medley.client/src/pages/GuildPage.vue
+++ Capstone-MTG-Medley.client/src/pages/GuildPage.vue
@@ -4,7 +4,10 @@
   </header>
   <section class="row">
     <div class="col-md-4">
-      Guild information
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d23118.78595558883!2d-116.30494195831875!3d43.58887719597092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1smagic%20the%20gathering!5e0!3m2!1sen!2sus!4v1662323201059!5m2!1sen!2sus"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <div class="col-md-3 offset-md-5 d-flex flex-column">
       <p class="p-0">
