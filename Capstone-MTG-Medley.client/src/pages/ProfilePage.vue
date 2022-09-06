b3276f12ac178415597b05feedbd9eeb74eda45d
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-06 10:43:00 -0600

    changes to profile page and service

--- Capstone-MTG-Medley.client/src/pages/ProfilePage.vue
+++ Capstone-MTG-Medley.client/src/pages/ProfilePage.vue
@@ -3,14 +3,21 @@
     <Navbar />
   </header>
 
-  {{ account }}
-
-
+  <section>
+    <div class="row">
+      <div class="profile-details rounded text-dark elevation-2">
+        <img src="" alt="" />
+        <div class="px-2">
+          <p>{{ profile.name }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import { computed } from "@vue/reactivity";
-import { onMounted } from "vue";
+import { onMounted, popScopeId } from "vue";
 import { useRoute } from "vue-router";
 import { AppState } from "../AppState";
 import { router } from "../router";
@@ -24,11 +31,9 @@ export default {
 
     async function getProfileById() {
       try {
-        console.log()
-        await profilesService.getProfileById(AppState.account.id)
+        await profilesService.getProfileById(route.params.profileId);
       } catch (error) {
-        logger.error(error);
-        Pop.toast(error.message, "error");
+        Pop.error("[Getting profile by Id]", error);
         router.push({ name: "Home" });
       }
     }
@@ -38,6 +43,7 @@ export default {
     });
 
     return {
+      route,
       account: computed(() => AppState.account),
       profile: computed(() => AppState.activeProfile),
     };
@@ -45,10 +51,4 @@ export default {
 };
 </script>
 
-<<<<<<< HEAD
-<style scoped lang="scss">
-</style>
-=======
-<style>
-</style>
->>>>>>> 9998b04489e7661f264ff3c06b1c92fbe25d0e50
+<style scoped lang="scss"></style>
