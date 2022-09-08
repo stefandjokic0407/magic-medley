5a732b1b4820f7b930b70c5a80714b5d777302fd
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-08 14:51:03 -0600

    deck rating server push

--- Capstone-MTG-Medley/server/models/Deck.js
+++ Capstone-MTG-Medley/server/models/Deck.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
+import { logger } from '../utils/Logger.js'
 
 const rating = new Schema({
-  value: {type: Number, min: 1, max: 5},
-  creatorId: {type: Schema.Types.ObjectId, ref: 'Account', required: true }
+  value: { type: Number, min: 1, max: 5 },
+  creatorId: { type: Schema.Types.ObjectId, ref: 'Account', required: true }
 })
 
 rating.virtual('creator', {
@@ -15,9 +16,9 @@ rating.virtual('creator', {
 
 export const DeckSchema = new Schema(
   {
-    picture: { type: String, default: 'https://preview.redd.it/dm8s72bg1zf51.jpg?width=1632&format=pjpg&auto=webp&s=77acaefbbfef40c240a13b7ef6006974b19cf6f9'},
-    name: {type: String, required: true, minlength: 1, maxlength: 50},
-    description: { type: String, maxlength: 5000},
+    picture: { type: String, default: 'https://preview.redd.it/dm8s72bg1zf51.jpg?width=1632&format=pjpg&auto=webp&s=77acaefbbfef40c240a13b7ef6006974b19cf6f9' },
+    name: { type: String, required: true, minlength: 1, maxlength: 50 },
+    description: { type: String, maxlength: 5000 },
     accountId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
     likeIds: { type: Array },
     likes: { type: Array },
@@ -34,16 +35,23 @@ DeckSchema.virtual('profile', {
   ref: 'Account'
 })
 
-DeckSchema.virtual('avgRating').get(()=>{
-  let sum = 0;
-  // @ts-ignore
- const arr = this.rating;
-  if(!arr){
+DeckSchema.virtual('avgRating')
+  .set(() => {
+    return 'testing stuff'
+  })
+  .get(function () {
+    let sum = null;
+    // @ts-ignore
+    const arr = this.rating
+    if (!arr) {
+      return sum
+    }
+    sum = 0
+    // for (const value of arr) {
+    for (let i = 0; i < arr.length; i++) {
+      logger.log('arr at i', i, arr[i])
+      // @ts-ignore
+      sum += arr[i].value;
+    }
     return sum
-  }
-// for (const value of arr) {
-  for (let i = 0; i <= arr.length; i++){
-    sum += arr[i].value;
-}
-return sum
-})
+  })
