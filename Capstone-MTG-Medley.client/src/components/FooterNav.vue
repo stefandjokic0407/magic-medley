ea86b657806152ab5759b2d5760cffb5d0b30dce
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-07 19:43:15 -0600

    footer works

--- Capstone-MTG-Medley.client/src/components/FooterNav.vue
+++ Capstone-MTG-Medley.client/src/components/FooterNav.vue
@@ -1,30 +1,24 @@
 <template>
-  <div class="row fixed-bottom footer-style justify-content-around text-uppercase text-my-light no-select">
+  <div
+    class="row fixed-bottom footer-style justify-content-around text-uppercase text-my-light no-select align-items-center">
     <!-- SECTION Account -->
-    <section class="col-1 py-2 text-center">
-      <div class="text-center border-bottom border-2 fs-5"><b>Account</b></div>
-      <div class="selectable">
-        <Login />
-      </div>
-      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#contactUsModal">
-        Contact Us
-      </div>
-    </section>
+    <div class="col-1 xsFont">
+      <Login />
+    </div>
+    <div class="col-1 text-my-light xsFont" data-bs-toggle="modal" data-bs-target="#contactUsModal">
+      Contact Us
+    </div>
 
     <!-- SECTION Information -->
-    <section class="col-1 py-2 text-center">
-      <div class="text-center border-bottom border-2 fs-5">
-        <b>Information</b>
-      </div>
-      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#faqsModal">
-        FAQs
-      </div>
-      <div class="text-my-light selectable">Site Tour</div>
-      <!-- NOTE modal is located in the contact us component -->
-      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#legalModal">
-        Legal
-      </div>
-    </section>
+
+    <div class="col-1 text-my-light xsFont" data-bs-toggle="modal" data-bs-target="#faqsModal">
+      FAQs
+    </div>
+    <div class="col-1 text-my-light xsFont">Site Tour</div>
+    <!-- NOTE modal is located in the contact us component -->
+    <div class="col-1 text-my-light xsFont" data-bs-toggle="modal" data-bs-target="#legalModal">
+      Legal
+    </div>
   </div>
   <ContactUsModal />
   <FaqsModalGroup />
@@ -51,5 +45,21 @@ export default {
   color: whitesmoke;
   text-shadow: 1px 1px 2px black;
   font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  cursor: pointer;
+}
+
+.deckText {
+  -webkit-text-stroke: .5px black;
+  color: rgba(255, 255, 255, 0.88);
+  text-shadow:
+    3px 3px 0 #000,
+    -1px -1px 0 #000,
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+}
+
+.xsFont {
+  font-size: x-small;
 }
 </style>
