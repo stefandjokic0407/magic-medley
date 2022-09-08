95f2cce04d35270a4778878a6b38f910c3032715
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-07 19:20:42 -0600

    home page not done yet

--- Capstone-MTG-Medley.client/src/components/FooterNav.vue
+++ Capstone-MTG-Medley.client/src/components/FooterNav.vue
@@ -1,41 +1,27 @@
 <template>
-  <div
-    class="row fixed-bottom footer-style justify-content-around text-uppercase text-my-light no-select"
-  >
+  <div class="row fixed-bottom footer-style justify-content-around text-uppercase text-my-light no-select">
     <!-- SECTION Account -->
-    <section class="col-3 py-2 text-center">
+    <section class="col-1 py-2 text-center">
       <div class="text-center border-bottom border-2 fs-5"><b>Account</b></div>
       <div class="selectable">
         <Login />
       </div>
-      <div
-        class="text-my-light selectable"
-        data-bs-toggle="modal"
-        data-bs-target="#contactUsModal"
-      >
+      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#contactUsModal">
         Contact Us
       </div>
     </section>
 
     <!-- SECTION Information -->
-    <section class="col-3 py-2 text-center">
+    <section class="col-1 py-2 text-center">
       <div class="text-center border-bottom border-2 fs-5">
         <b>Information</b>
       </div>
-      <div
-        class="text-my-light selectable"
-        data-bs-toggle="modal"
-        data-bs-target="#faqsModal"
-      >
+      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#faqsModal">
         FAQs
       </div>
       <div class="text-my-light selectable">Site Tour</div>
       <!-- NOTE modal is located in the contact us component -->
-      <div
-        class="text-my-light selectable"
-        data-bs-toggle="modal"
-        data-bs-target="#legalModal"
-      >
+      <div class="text-my-light selectable" data-bs-toggle="modal" data-bs-target="#legalModal">
         Legal
       </div>
     </section>
@@ -54,10 +40,11 @@ export default {
 
 <style lang="scss" scoped>
 .footer-style {
-  background: rgb(0 0 0 / 38%);
+  background: rgba(0, 0, 0, 0.721);
   backdrop-filter: blur(4px);
   border: solid #8d8b8b1f;
   border-radius: 8px;
+  height: 5VH;
 }
 
 .text-my-light {
