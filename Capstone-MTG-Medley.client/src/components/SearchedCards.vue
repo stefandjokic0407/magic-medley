7b7f2865fb510dfb968fe5892ca029a6b51040ed
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-09 15:28:42 -0600

    minor style edit

--- Capstone-MTG-Medley.client/src/components/SearchedCards.vue
+++ Capstone-MTG-Medley.client/src/components/SearchedCards.vue
@@ -6,7 +6,7 @@
     :data-bs-target="'#cardModal' + card.id"
     class="shadow rotated"
   >
-    <div class="card-bg">
+    <div class="bg-dark">
       <div class="magic-card" :class="{ loaded: imgLoaded }">
         <img
           @load="imgLoaded = true"
