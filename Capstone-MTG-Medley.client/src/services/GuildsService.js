6d5af4f8441bd0ef05b53a8e81e8b826b05c0b20
“stefandjokic0407” <stefandjokic0407@gmail.com>
2022-09-09 22:30:29 -0600

    Got the profile guild info

--- Capstone-MTG-Medley.client/src/services/GuildsService.js
+++ Capstone-MTG-Medley.client/src/services/GuildsService.js
@@ -18,6 +18,14 @@ class GuildsService {
     const res = await api.get(`api/guilds/${guildId}`)
     AppState.activeGuild = res.data
   }
+
+  async getProfileGuild(){
+  const res = await api.get(`account/members/`)
+  console.log(res.data)
+
+}
+
+
 }
 
 export const guildsService = new GuildsService()
